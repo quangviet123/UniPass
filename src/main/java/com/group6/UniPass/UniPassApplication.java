15df6c27b1d8f9c6d38a7f0bfabb6dedf3525163
Trần Quang Việt <hhhyyy584@gmail.com>
2026-02-06 11:52:34 +0700

    init project

---
+++ src/main/java/com/group6/UniPass/UniPassApplication.java
@@ -0,0 +1,13 @@
+package com.group6.UniPass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniPassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniPassApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/group6/UniPass/UniPassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group6.UniPass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniPassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
